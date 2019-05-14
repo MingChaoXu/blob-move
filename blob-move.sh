b5be4153cbf149a25601dde47d72a1859eba0db9
MingChaoXu <xumingchao_2013@163.com>
2019-05-14 23:27:24 +0800

    add new

--- blob-move.sh
+++ blob-move.sh
@@ -2,3 +2,6 @@ npm install hexo-deployer-git --save
 npm install hexo-neat --save
 npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
+npm install --save hexo-generator-index
+npm install --save hexo-generator-archive
+npm install --save hexo-generator-tag
